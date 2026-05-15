ec1e28356403fe31df58b8b30d3c47930c904fd2
Goutham <goutham27072006@gmail.com>
2026-05-15 19:52:41 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="quizapp",
+    version="1.0",
+    description="Quiz Application Package",
+    author="Your Name",
+    packages=find_packages()
+)
